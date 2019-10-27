74a5525ce0a7b6be9ec57f9ad9083bca25d42552
alexmaslarski <38736057+alexmaslarski@users.noreply.github.com>
2019-10-27 17:53:25 +0100

    Firebase

---
+++ js/firebase_config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCLslndZkWA1MD4hdozGIjw_UNp9_LAM7o",
+    authDomain: "arla-project.firebaseapp.com",
+    databaseURL: "https://arla-project.firebaseio.com",
+    projectId: "arla-project",
+    storageBucket: "arla-project.appspot.com",
+    messagingSenderId: "487132783360",
+    appId: "1:487132783360:web:b0a0404a8dae399662c9e5",
+    measurementId: "G-VXFXKFKYN5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
